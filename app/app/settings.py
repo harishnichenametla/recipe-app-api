2590b6c378114ac0a92abe29ded8b8f11550b6e4
harish <hareeshla88@gmail.com>
2021-01-15 16:46:47 +0530

    added command

--- app/app/settings.py
+++ app/app/settings.py
@@ -74,13 +74,28 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST' : os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'), 
     }
 }
 
+print(os.environ)
+print(os.environ.get('DB_HOST'))
+print(os.environ.get('DB_NAME'))
+print(os.environ.get('DB_USER'))
+print(os.environ.get('DB_PASS'))
+print('finish')
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
